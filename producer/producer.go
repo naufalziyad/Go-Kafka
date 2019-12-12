d80aaf6ccb11a9d7ffcf0809bb65b8be41ee42b4
Naufal Ziyad <naufal.ziyad@detik.com>
2019-12-12 11:03:06 +0700

    [update] kafka producer , for test

--- producer/producer.go
+++ producer/producer.go
@@ -1,17 +1,30 @@
-package producer 
+package producer
+
 import (
 	"fmt"
 
 	"github.com/Shopify/sarama"
+	"github.com/sirupsen/logrus"
 )
 
 type KafkaProducer struct {
 	Producer sarama.SyncProducer
 }
 
-func (p *KafkaProducer) SendMessage(topic, msg string) error{
+func (p *KafkaProducer) SendMessage(topic, msg string) error {
 	fmt.Println("coba saja")
+	kafkaMessage := &sarama.ProducerMessage{
+		Topic: topic,
+		Value: sarama.StringEncoder(msg),
+	}
+
+	paritition, offset, err := p.Producer.SendMessage(kafkaMessage)
+	if err != nil {
+		logrus.Errorf("Send Message Error : %v", err)
+		return err
+
+	}
+
+	logrus.Infof("Send Message Success, Topic %v, Paritition %v, Offset %d", topic, paritition, offset)
 	return nil
 }
-
-
